5dfa230a27e181f55cfa7586cd18fdbf53d78838
JavierSanz91 <126878939+JavierSanz91@users.noreply.github.com>
2026-03-25 06:54:19 +0100

    Add src/index.ts

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+export { ScoreBadge } from './components/ScoreBadge'
+export { MetricCard } from './components/MetricCard'
+export { TripleCheck } from './components/TripleCheck'
+export { PillarGauge } from './components/PillarGauge'
+export { SensitivityTable } from './components/SensitivityTable'
+
+export { scoreColor, scoreBg, formatNumber, formatPct } from './utils'
+
+export type { ScoreBadgeProps, MetricCardProps, TripleCheckProps, PillarScores } from './types'
